a2ca759c68d2b03f35e735860b8f60b3205faf68
thanhbr39@gmail.com <thanhbr39@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-06 07:25:08 +0000

    [ThanhTC3] code adminpredict.php

---
+++ BLL/matchlistBll.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @author thanh
+ * @copyright 2013
+ */
+ 
+function getMatchList($typeLeaguesId) {
+    $sql = "SELECT * FROM tbl_matchList Where TypeLeaguesId = $typeLeaguesId AND Result = ''";
+    $queryResult = mysql_query($sql);
+    $i = 0;
+    $result = array();
+    while ($seletedItem = mysql_fetch_array($queryResult)) {
+        $item = new Matchlist();
+        $item->Id = $seletedItem['Id'];
+        $item->ClubA = $seletedItem['ClubA'];
+        $item->ClubB = $seletedItem['ClubB'];
+        $item->StartTime = $seletedItem['StartTime'];
+        $item->Result = $seletedItem['Result'];
+        $result[$i] = $item;
+        $i++;
+    }
+    return $result;
+}
+
+function updateResult($id, $result) {
+    $result = mysql_real_escape_string($result);
+//    echo '<script>alert("'.$result.'");</script>';
+    $sql= "
+    UPDATE `footballpredict`.`tbl_matchlist` SET `Result` = '$result' WHERE id = $id;";
+    $queryResult = mysql_query($sql);
+    if (!$queryResult) {
+        echo 'Error: ' . $id . mysql_error();
+        return -1;
+    }
+}
+
+?>
