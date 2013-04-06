4222356b656e5dd5fb6d6d6c71bce244091fb896
thanhbr39@gmail.com <thanhbr39@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-06 10:35:23 +0000

    [ThanhTC3] code adminpredict.php

--- trunk/loginpanel.php
+++ trunk/loginpanel.php
@@ -64,15 +64,69 @@
                 <span class="cont-title-sub"></span>
                 <i class="sub0"></i>
             </div>
-            <span class="popup-error-mess"><i></i> Input is not correct!</span>
-            <div class="popup-input-row"><span>Full Name</span><input type="text"/></div>
-            <div class="popup-input-row"><span>Email</span><input type="text"/></div>
-            <div class="popup-input-row"><span>Confirm</span><input type="text" placeholder="email"/></div>
-            <div class="popup-input-row"><span>Password</span><input type="password"/></div>
-            <div class="popup-input-row"><span>Confirm</span><input type="password" placeholder="password"/></div>
-            <div class="popup-btn-row">
-                <a class="popup-btn-login" id="btn-register" href="#">Register</a>
-            </div>
+            <form method="post">
+                <?php
+                    include 'BLL/userBll.php';
+                    //----------------------------
+                    
+                    if(isset($_POST['btnRegis'])) {
+                        $errors = array();
+                        if(empty($errors)) {
+                            $name = mysql_real_escape_string($_POST['name']);
+                            $email = mysql_real_escape_string($_POST['email']);
+                            $pword = mysql_real_escape_string($_POST['pword']);
+                            $avatar = "images/resources/team-logo/Alania.jpg";
+                            $bod = "2000-01-01";
+                            //$gender = mysql_real_escape_string($_POST['sex']);
+                            $gender = "male";
+                            if($gender === male ) {
+                                $gender = 1;
+                            } else {
+                                $gender = 0;
+                            }
+                            $emailExist = emailExist($email);
+                            if($emailExist == 1) {
+                                $errors[] = "Email exist.";
+                            }
+                            
+                            if(empty($errors)) {
+                                $result = insertUser ($name, $email, $pword, $bod, $gender, $avatar);
+                                if($result > 0) {
+                                    
+                                    $_SESSION['UserName'] = $user->FullName;
+                                    $_SESSION['UserId'] = $result;
+                                    $_SESSION['UserAvatar'] = $avatar;
+                                    
+                                } else {
+                                    $errors[] = "Can not create user.";
+                                }
+                            }
+                           
+                        }
+                    
+                    }//End if($_POST['submit'])}
+                ?>
+                <?php if(!empty($errors)) {
+                echo "<ul>";
+                foreach($errors as $error) {
+                    echo "<span class='popup-error-mess'><i></i>{$error}</span>";
+                    
+                }
+                echo "</ul>";
+            }
+        ?>
+                
+                
+                <div class="popup-input-row"><span>Full Name</span><input type="text" name="name"/></div>
+                <div class="popup-input-row"><span>Email</span><input type="text" name="email"/></div>
+                <div class="popup-input-row"><span>Confirm</span><input type="text" placeholder="email" name="cemail"/></div>
+                <div class="popup-input-row"><span>Password</span><input type="password" name="pword"/></div>
+                <div class="popup-input-row"><span>Confirm</span><input type="password" placeholder="password"/></div>
+                <div class="popup-btn-row">
+                    <input type='submit' class="popup-btn-login" id="btn-register" name='btnRegis' value='Register'/>
+                </div>
+            </form>
+           
             <span class="loading-spin undisplayed"></span>
         </div>
         <div class="popup-bottom"></div>
