e963ad93884ff8ed0736e291702c7d24bfbb3628
thanhbr39@gmail.com <thanhbr39@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-10 14:25:16 +0000

    [ThanhTC3] fix bug

--- trunk/faq.php
+++ trunk/faq.php
@@ -37,32 +37,70 @@
                             <i class="sub0"></i>
                         </div>
                         <div class="page-cont-text">
-                            <h3>How does the scoring work?</h3>
+							<h3>Are you a real football fan?</h3>
+							<h3>Are you eager to prove that to your friends?</h3>
+							</br>
+							<p>Football Challenge allows you to predict the results of your favorite football games.</p>
+							<p>The better you predict, the more points you will get.</p>
+							<p>Try your best, beat your friends and get to the top of the ranking!</p>
+							
+                            <h3>1. How to register?</h3>
+							<p>You can register through your Facebook account through a simple click.  As an alternative, click </p>
+							<p>here or the “Registration” link located on the main page.  We don't need too much information </p>
+							<p>from you -- just your name, preferred username, password, date of birth, favorite football </p>
+							<p>team, avatar, and e-mail address.  Once you're registered, you're ready to begin playing!</p>
+							
+							<h3>2. What I am playing for?</h3>
+							<p>At this time, Football Challenge is just for your pure enjoyment (it's so much fun, right?!).  In  </p>
+							<p>the future we will be able to provide free prizes to those users who frequently visit our site </p>
+							<p>and to those users who are great at predicting game outcomes!  Stick around for that day to come.</p>
+							
+							<h3>3. How do I play?</h3>
+							<p>Choose the tab</p> <h3>“My Predictions”</h3> <p>and add your respective predictions for the following week. </p>
+							<p>You can scroll down and submit your predictions for future rounds as well, but can </p>
+							<p>change/update them in due time. Your predictions will be considered final as of 24 hours </p>
+							<p>before each game begins. Make sure you have submitted your results by then, or else you</p>
+							<p>won’t get any points for that round! You will receive a notification email every week letting</p>
+							<p>you know that you can submit the results for the following week.</p>
+							
+							<h3>4. Point system</h3>
                             <p>You earn points for:
                             </p><ul>
-                                <li>Proper home team score: +5</li>
-                                <li>Proper away team score: +5</li>
-                                <li>Proper winner or tie: +20</li>
-                                <li>Bonus for correct outcome: +10</li>
+                                <li>Proper home team score: +10</li>
+                                <li>Proper away team score: +10</li>
+                                <li>Proper winner or tie: +40</li>
+                                <li>Correct outcome +20 bonus points</li>
                             </ul>
-                            <h3>Until when can I change my prediction?</h3>
-                            <p>You can change your predictions as often as you want until the start of the match.</p>
-                            <h3>Can I change my favorite team setting</h3>
-                            <p>Yes you can: you'll find this option in the Settings page (when you are logged in).</p>
-                            <h3>How do I create my own pool?</h3>
-                            <p>Click on the tab 'Pools' on top of the homepage and then 'Create new pool'</p>
+							<p>Your points will be added after each round until the end of the Russian championship</p>
+							
+							
+							
+                            <h3>5. Can I invite my friends?</h3>
+                            <p>Yes you can! Just click on the 'invite friends' button at the top right of the home page and invite </p>
+                            <h3>your friends. You can type their e-mail addresses or if you logged in by your Facebook account, you </h3>
+                            <p>can select them from your Facebook friends.</p>
+							
+							
+							
+                            <h3>6. Can I create a group?</h3>
+                            <p>Yes you can! It is very easy and fun to create your own group and have your group ranking to</p>
+							
+							<p>compete against the group members. To create a group first you should go to "Groups" tab and </p>
+							<p>under this tab you should click on "My groups". Here you can see the list if your own groups. To </p>
+							<p>create a new group you just need to click on "Create a new group" button. All you need to do is </p>
+							<p>pick a name and logo for your group and then invite your friends either by typing their e-mail </p>
+							<p>addresses or through your Facebook account. You can either create an open or closed group. Open </p>
+							<p>groups will be visible under the global groups list and other members will be able to join your </p>
+							<p>group. Closed groups are going to be open only people who were invited by the members of that </p>
+							<p>group. (Note for the developers: group names should be unique, so that system should check the </p>
+							<p>names and warn the user if the name is not available anymore).</p>
+							
 
-                            <h3>What is a private pool?</h3>
-                            <p>When creating a pool you can choose to make it private or public:
-                            </p><ul>
-                                <li>In private pools only the person who started the group can invite friends;</li>
-                                <li>In public pools anyone can invite friends and participants can also sign up without invitation;</li>
-                                <li>In both pools only the person who started the pool can remove participants.</li>
-                            </ul>
-                            <h3>Do you have another question?</h3>
+                            <h3>7. How can I join a group?</h3>
+                            <p>Go to "Groups" tab. Here you will see the list of all groups. (If they are open to public). All you need </p>
+							<p>to do is click in "Join this group" button under the logo if that group.</p>
                         </div>
                         <div class="page-cont-ref faq"></div>
-                        <div class="page-home-row-btn faq">Send us a message</div>
 
                     </div>
                 </div>
