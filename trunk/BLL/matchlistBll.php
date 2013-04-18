c52bbda00c2469efd8b676ee755794e2b669e5eb
thanhbr39@gmail.com <thanhbr39@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-18 19:28:44 +0000

    [ThanhTC3] update admin

--- trunk/BLL/matchlistBll.php
+++ trunk/BLL/matchlistBll.php
@@ -23,6 +23,24 @@ function getMatchList($typeLeaguesId) {
     return $result;
 }
 
+function getAdminMatchList($typeLeaguesId) {
+    $sql = "SELECT * FROM tbl_matchlist Where TypeLeaguesId = '".$typeLeaguesId."' AND Result = '' AND StartTime BETWEEN '' AND (SELECT ADDDATE(CURDATE(), INTERVAL 3 DAY))";
+    $queryResult = mysql_query($sql);
+    $i = 0;
+    $result = array();
+    while ($seletedItem = mysql_fetch_array($queryResult)) {
+        $item = new Matchlist();
+        $item->Id = $seletedItem['Id'];
+        $item->ClubA = $seletedItem['ClubA'];
+        $item->ClubB = $seletedItem['ClubB'];
+        $item->StartTime = $seletedItem['StartTime'];
+        $item->Result = $seletedItem['Result'];
+        $result[$i] = $item;
+        $i++;
+    }
+    return $result;
+}
+
 function updateResult($id, $result) {
     $result = mysql_real_escape_string($result);
 //    echo '<script>alert("'.$result.'");</script>';
