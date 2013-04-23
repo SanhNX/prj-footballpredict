f1c1f7410373ea51ca057e765e0d5db1f5e42568
sanh23021987@gmail.com <sanh23021987@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-23 15:54:18 +0000

    - add google ads to right panel

--- index.php
+++ index.php
@@ -52,7 +52,19 @@
             include 'headerpanelindex.php';
             ?>
             <div class="cover home">
-
+                <div class="google-ads-left">
+                    <script type="text/javascript"><!--//<![CDATA[
+                    google_ad_client = "ca-pub-5425751846726392";
+                        /* skyscraper120x600 */
+                        google_ad_slot = "4658062125";
+                        google_ad_width = 120;
+                        google_ad_height = 600;
+                        //]]>//-->
+                    </script>
+                    <script type="text/javascript"
+                            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+                    </script>
+                </div>
                 <div class="google-ads-right">
                     <script type="text/javascript"><!--//<![CDATA[
                     google_ad_client = "ca-pub-5425751846726392";
