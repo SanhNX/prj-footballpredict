1ad631c7a2e7616ba18d559512d758b1bdcf6039
sanh23021987@gmail.com <sanh23021987@gmail.com@f74dc153-cffe-b25a-db3a-0d5c052981e4>
2013-04-26 13:12:20 +0000

    - add site close

---
+++ site-close.php
@@ -0,0 +1,20 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <!--[if !IE]><!-->
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <!--<![endif]-->
+        <!--[if lt IE 9]>
+        <link rel="stylesheet" type="text/css" href="css/style-ie8.css" />
+        <![endif]-->
+    </head>
+    <body>
+        <div class="site-close"></div>
+    </body>
+</html>
